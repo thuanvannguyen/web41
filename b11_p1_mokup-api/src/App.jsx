7edfffa16e370d258f1527b457e90366fd3e77d9
ThuanVan Nguyen <60531224+thuanvannguyen@users.noreply.github.com>
2025-03-28 20:41:10 +0700

    Update b11

---
+++ b11_p1_mokup-api/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <h1>Mockup API Running...</h1>
+  )
+}
+
+export default App
---
+++ b11_p2-redux-basic/src/main.jsx
@@ -0,0 +1,74 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { createStore } from "redux";
+
+// Khoi tao gia tri ban dau
+const initialState = {
+  count: 0,
+};
+
+// Dinh nghia action type & action creator
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+const incrementFn = () => {
+  return { type: INCREMENT };
+};
+
+const decrementFn = () => {
+  return { type: DECREMENT };
+};
+
+// Dinh nghia store & reducer
+const counterReducer = (state = initialState, action) => {
+  // console.log("state: ", state);
+  // console.log("action: ", action);
+
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const myStore = createStore(counterReducer);
+
+// Hien thi ra giao dien
+const App = () => {
+  const couter = useSelector((state) => state);
+  console.log(couter);
+
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(incrementFn());
+  };
+  const handleDecrement = () => {
+    dispatch(decrementFn());
+  };
+
+  return (
+    <>
+      <h1>{couter.count}</h1>
+      <button onClick={handleIncrement}>Count++</button>
+      <button onClick={handleDecrement}>Count--</button>
+    </>
+  );
+};
+
+// Khai bao store voi provider de tat ca component con co the dung no => Duoc dat o thanh phan cao nhat
+createRoot(document.getElementById("root")).render(
+  <Provider store={myStore}>
+    <App />
+  </Provider>
+);
