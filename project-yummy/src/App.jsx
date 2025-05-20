badb233cd55fa2d4f79e2012933c485d3a78edc5
ThuanVan Nguyen <nguyenthuankma@gmail.com>
2025-05-20 21:44:18 +0700

    Update Router config

--- project-yummy/src/App.jsx
+++ project-yummy/src/App.jsx
@@ -6,6 +6,7 @@ import LayoutPage from "./pages/layout";
 import Dashboad from "./pages/dashboard";
 import NotFound from "./pages/auth/NotFound";
 import ProtectedRoute from "./pages/auth/ProtectedRoute";
+import DashboardLayout from "./pages/layout/Dashboard";
 
 const App = () => {
   return (
@@ -14,17 +15,22 @@ const App = () => {
         <Route path="/" element={<LayoutPage />}>
           <Route index element={<HomePage />} />
 
-
           <Route path="/login" element={<LoginPage />} />
 
-          <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Dashboad />
-            </ProtectedRoute>
-            } />
-
-
           <Route path="*" element={<NotFound />} />
+        </Route>
+
+        <Route
+          path="/dashboard/*"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Dashboad />} />
+          <Route path="user" element={<p>User</p>} />
+          <Route path="*" element={<p>404 NotFound</p>} />
         </Route>
       </Routes>
     </BrowserRouter>
